83a7c8cdde7f9bf1d3801b88bf1a9ba9c8077923
autotel <joaquin@autotel.co>
2017-04-10 11:24:52 +0900

    handler's 'this' is now caller
    #beware that updating from this version has some functionality changes
    -instead of just running the queued callback, onhandler will make handler.call(this,args) ensuring a more useful context for the call
    -addded but commented out the "once" function because it has not been throroughly tested. You can uncomment it to try and put comments"

--- on.js
+++ on.js
@@ -1,10 +1,10 @@
 /*
-you make the onHandlers.call(this) in the object that needs to have handlers.
-then you can create a function callback for that object using object.on("handlerName.optionalName",callbackFunction(){});
-the object can run the handle callbacks by using this.handle("handlerName",parametersToFeed);
+repository of this module is at
+https://github.com/autotel/on
 */
 module.exports=function() {
   var eventVerbose=false;
+  var owner=this;
   if (!this.ons) {
     this.ons = [];
   }
@@ -21,7 +21,23 @@ module.exports=function() {
     } else {
       throw ("error at mouse.on, provided callback that is not a function");
     }
+    return this;
   }
+  // this.once = function(name, callback) {
+  //   var name = name.split(".");
+  //   if (typeof callback === 'function') {
+  //     if (name.length == 0) {
+  //       throw ("sorry, you gave an invalid event name");
+  //     } else if (name.length > 0) {
+  //       if (!this.ons[name[0]]) this.ons[name[0]] = [];
+  //       this.ons[name[0]].push([true, callback]);
+  //     }
+  //     // console.log(this.ons);
+  //   } else {
+  //     throw ("error at mouse.on, provided callback that is not a function");
+  //   }
+  //   return this;
+  // }
   this.off = function(name) {
     var name = name.split(".");
     if (name.length > 1) {
@@ -37,8 +53,17 @@ module.exports=function() {
     if(eventVerbose) console.log("Event "+fname+":",{caller:this,params:params});
     if (this.ons[fname]) {
       for (var n in this.ons[fname]) {
-        // console.log(this.ons[fname][n][1]);
-        this.ons[fname][n][1](params);
+        //pendant: how to make a better error handling? otherwise the function is bubbling the error to the handle() caller!!
+        try{
+          // console.log(this.ons[fname][n][1]);
+          this.ons[fname][n][1].call(owner,params);
+          //if the flag is set to do once
+          if(this.ons[fname][n][0]){
+            this.ons[fname].splice(this.ons[fname][n], 1);
+          }
+        }catch(e){
+          throw ("onHandler: error with "+fname+" callback:",e);
+        }
       }
     }
   }
