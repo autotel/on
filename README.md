c775e92121d99fc0268704506bc43f90fa6b7c97
Joaquín Aldunate <joaquin@autotel.co>
2018-06-07 16:34:34 +0300

    added support for reference removal of listeners
    also fixed removal of listeners in name basis.
    improved and updated the example

--- README.md
+++ README.md
@@ -2,42 +2,63 @@
 function that easily adds the "on" listener to any object, in a jQuery fashion.
 you can make any object to have the "on" functionality when it calls() onHandlers:
 ```javascript
-onHandlers.call(this);
+OnHandlers.call(this);
 ```
 
 then you can just add the handling points in the object.
 ##example using HTML or making on a global
 You need to modify the on.js source code, in the first line instead of being `module.exports=function(){`, it should be `onHandlers=function(){`
 ```html
-<script src="on.js"></script>
-<script>
-console.log("click in the document");
-//imagine we are making some sort of javascript library
-var testob=(function(){
-  //I make this just to get the "this" within a listener callback
-  var parent=this;
-  //whoever calls the onHandlers, gets the handling functionality
-  onHandlers.call(this);
-  document.addEventListener("click",function(){
-    //by doing this single line, the testob effectively has a "clik" listener.
-    parent.handle("clik");
-  });
-  this.do=function(){
-    //by doing this single line, the testob effectively has a "doing" listener.
-    this.handle("doing");
-  }
-  return this;
-})();
-//now we create the callback for the listeners.
-testob.on("doing",function(){
-  console.log("testob is doing!");
-});
-testob.on("clik",function(){
-  console.log("testob is handling a click.... let's make it do()");
-  testob.do();
-});
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>On example</title>
+  </head>
+  <body>
+    
+      click in the document.
+    <div id="console">
 
-</script>
+    </div>
+ </body>
+  <script src="on.js"></script>
+  <script>
+  console.log("click in the document");
+  var consoleEl=document.getElementById('console');
+  var testob=(function(){
+    var self=this;
+    this.count=10;
+    OnHandlers.call(this);
+    document.addEventListener("click",function(e){
+      e.preventDefault();
+      self.handle("click");
+      count--;
+    });
+    this.do=function(){
+      this.handle("doing");
+    }
+    return this;
+  })();
+  
+  testob.on("click",function(){
+    consoleEl.innerHTML+="<br>testob is handling a click. "+testob.count+" clicks to disable by name";
+    if(testob.count<1){
+      //you can disable events by event name, affecting every listener under that name
+      testob.off("click");
+    }
+  });
+
+  var refListener=testob.on("click",function(){
+    // you can also disable events by reference, which will not affect the other events
+    consoleEl.innerHTML+="<br>testob handler 2. "+(testob.count-5)+" clicks to disable by reference";
+    if(testob.count<6){
+      testob.off(refListener);
+    }
+  });
+
+  </script>
+</html>
 ```
 ##example using modular syntax
 ```javascript
--- on.js
+++ on.js
@@ -1,83 +1,63 @@
 /*
-repository of this module is at
-https://github.com/autotel/on
+you make the onHandlers.call(this) in the object that needs to have handlers.
+then you can create a function callback for that object using object.on("handlerName.optionalName",callbackFunction(){});
+the object can run the handle callbacks by using this.handle("handlerName",parametersToFeed);
 */
-module.exports=function() {
+var OnHandlers=function() {
   var eventVerbose=false;
-  var debugVerbose=false;
-  var owner=this;
+  var self=this;
+  var CallbackObject=function(callback){
+    this.callback=callback;
+  }
   if (!this.ons) {
-    //this.ons[evtName][evtSurname|0]=[evt]
-    //evt=[removeFlag,callback]
     this.ons = [];
   }
   this.on = function(name, callback) {
-    var sname = name.split(".");
+    var name = name.split(" ");
     if (typeof callback === 'function') {
-      if (sname.length == 0) {
+      if (name.length == 0) {
         throw ("sorry, you gave an invalid event name");
-      } else if (sname.length == 1) {
-        if (!this.ons[name]) this.ons[name] = [];
-        if (!this.ons[name][0]) this.ons[name][0] = [];
-        this.ons[name][0].push([false, callback]);
-        if(debugVerbose) console.log("add"+name+"[0]",callback,this.ons[name]);
-      } else if (sname.length > 1) {
-        if (!this.ons[sname[0]]) this.ons[sname[0]] = [];
-        if (!this.ons[sname[0]][sname[1]]) this.ons[sname[0]][sname[1]] = [];
-        this.ons[sname[0]][sname[1]].push([false, callback]);
-        if(debugVerbose) console.log("add"+sname[0]+"["+sname[1]+"]",callback,this.ons[name]);
+        return false;
+      } else if (name.length > 0) {
+        var unique=new CallbackObject(callback);
+        for(var evtName of name){
+          if (!self.ons[evtName]) self.ons[evtName] = [];
+          self.ons[evtName].push(unique);
+        }
+        return unique;
       }
-      // console.log(this.ons);
     } else {
-      throw ("error at event handler, provided callback that is not a function");
+      throw ("error at mouse.on, provided callback that is not a function");
+      return false;
     }
-    return this;
   }
-  // this.once = function(name, callback) {
-  //   var name = name.split(".");
-  //   if (typeof callback === 'function') {
-  //     if (name.length == 0) {
-  //       throw ("sorry, you gave an invalid event name");
-  //     } else if (name.length > 0) {
-  //       if (!this.ons[name[0]]) this.ons[name[0]] = [];
-  //       this.ons[name[0]].push([true, callback]);
-  //     }
-  //     // console.log(this.ons);
-  //   } else {
-  //     throw ("error at mouse.on, provided callback that is not a function");
-  //   }
-  //   return this;
-  // }
   this.off = function(name) {
-    var sname = name.split(".");
-    if (name.length == 1) {
-      delete this.ons[sname[0]][0];
-    } else if (name.length > 1) {
-      //remove all events that have been attached by addding name such as mouseover.ball
-      delete this.ons[sname[0]][sname[1]];
+    if (typeof name == "string") {
+      var nameList=name.split(" ");
+      for(var name of nameList){
+        this.ons[name] = [];
+      }
+    } else if (name instanceof CallbackObject) {
+      //console.log("off by ref");
+      for(var evtListNameItem in self.ons){
+        //console.log(evtListNameItem)
+        for(var evtNum in self.ons[evtListNameItem]){
+          //console.log(evtNum);
+          if(name===self.ons[evtListNameItem][evtNum]) self.ons[evtListNameItem].splice(evtNum,1);
+        }
+      }
     } else {
-      throw ("sorry, you gave an invalid event name: " + name);
+      throw ("sorry, you gave an invalid listener reference: ",name);
     }
-    if(debugVerbose) console.log("rm",name,this.ons);
   }
   this.handle = function(fname, params) {
     if(eventVerbose) console.log("Event "+fname+":",{caller:this,params:params});
-    if (this.ons[fname]) {
-      for (var surname in this.ons[fname]) {
-        for(var n in this.ons[fname][surname]){
-          //pendant: how to make a better error handling? otherwise the function is bubbling the error to the handle() caller!!
-          try{
-            // console.log(this.ons[fname][n][1]);
-            this.ons[fname][surname][n][1].call(owner,params);
-            //if the flag is set to do once
-            if(this.ons[fname][surname][n][0]){
-              this.ons[fname][surname].splice(this.ons[fname][surname][n], 1);
-            }
-          }catch(e){
-            throw ("onHandler: error with "+fname+" callback:",e);
-          }
-        }
+    if (self.ons[fname]) {
+      for (var n in self.ons[fname]) {
+        // console.log(self.ons[fname][n][1]);
+        self.ons[fname][n].callback(params);
       }
     }
   }
 };
+module.exports=OnHanlders;
